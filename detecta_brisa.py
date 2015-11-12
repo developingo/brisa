9ade6788485c5b2aa153c2a7816db7344f41599a
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2015-11-11 20:05:10 -0600

    save as CSV the result set

--- detecta_brisa.py
+++ detecta_brisa.py
@@ -33,6 +33,8 @@ if __name__ == '__main__':
 	amplitud = float(archivo_angulos.readline())
 	estacion_lon = float(archivo_coords.readline())
 
+	result_csv = csv.writer(open('%s/result.csv'%estacion, 'w'))
+
 	citla_csv = csv.reader(archivo_datos)
 
 	veracruz_lon = -96.15333333333334
@@ -60,20 +62,24 @@ if __name__ == '__main__':
 
 	for fila in citla_csv:
 		anio   = int(fila[0])
-		mes    = int(fila[1]) - 1
-		dia    = int(fila[2]) - 1
+		mes    = int(fila[1])
+		dia    = int(fila[2])
 		hora   = int(fila[3])
 		minuto = int(fila[4])
 
 		angulo     = float(fila[5])
+		intensidad = float(fila[7])
 
-		hora_actual = datetime(anio, mes+1, dia+1, hora, minuto, 0, tzinfo=tz)
+		hora_actual = datetime(anio, mes, dia, hora, minuto, 0, tzinfo=tz)
 
-		fecha = '%d-%d-%d'%(anio, mes+1, dia+1)
+		fecha = '%d-%d-%d'%(anio, mes, dia)
 		amanecer  = datetime.strptime(sunrise_data[fecha]['sunrise'], '%Y-%m-%d %H:%M:%S%z') + offset
 		atardecer = datetime.strptime(sunrise_data[fecha]['sunset'], '%Y-%m-%d %H:%M:%S%z') + offset
 
 		if angulo != -999.90:
+			datos_hoy.append([
+				anio, mes, dia, angulo, intensidad
+			])
 			# Detectar si es de día
 			if hora_actual >= amanecer+dos and hora_actual < atardecer+dos:
 				# es de día
@@ -93,12 +99,14 @@ if __name__ == '__main__':
 
 		if hora == 23 and minuto == 50:
 			# final del día, revisamos si hubo brisa hoy
+			print('.', end='')
+			sys.stdout.flush()
 
-			mensaje = 'no'
+			mensaje = 0
 			if dia_actual['noD'] > 0 and dia_actual['tierramar']/dia_actual['noD'] > .6 and dia_actual['C2']:
-				mensaje = 'SI<---'
+				mensaje = 1
 
-			print(anio, mes+1, dia+1, mensaje, sep='\t')
+			result_csv.writerow([anio, mes, dia, mensaje])
 
 			dia_actual = {
 				'noD': 0,
@@ -106,3 +114,5 @@ if __name__ == '__main__':
 				'martierra': 0,
 				'C2': False,
 			}
+
+	print()
